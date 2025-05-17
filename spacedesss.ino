d468df9c78ab2ae38b13a6776775f08f68db07e7
CranenaZ <164602421+CranenaZ@users.noreply.github.com>
2025-05-17 20:14:51 +0800

    Add files via upload

---
+++ spacedesss.ino
@@ -0,0 +1,95 @@
+#include <SPI.h>
+#include <SdFat.h>
+#include <Wire.h>
+#include <SoftwareSerial.h>
+#include <TinyGPS++.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_HMC5883_U.h>
+#include <Adafruit_MPU6050.h>
+#include "MAX471.h"
+
+SdFat SD;
+const uint8_t CS_PIN = 10;
+SoftwareSerial gpsSerial(2, 3); // RX, TX for GPS
+TinyGPSPlus gps;
+Adafruit_HMC5883_Unified mag(12345);
+Adafruit_MPU6050 mpu;
+MAX471 myMax(ADC_10_bit, VCC_BAT, A1, A0);
+
+uint8_t pkt[27];
+char hexOut[55];
+unsigned long lastT;
+
+void pack(uint32_t v,int b,int &p){while(b--){int i=p>>3,bit=7-(p&7);pkt[i]|=((v>>b)&1)<<bit;p++;}}
+
+void build(){
+  memset(pkt,0,sizeof(pkt));
+  int p=0;
+  sensors_event_t e;
+  mag.getEvent(&e);
+  float ms[3]={e.magnetic.x*0.92,e.magnetic.y*0.92,e.magnetic.z*0.92};
+  for(int i=0;i<3;i++){pack(ms[i]<0,1,p);pack(min(127,int(abs(ms[i])*3)),7,p);}
+
+  Wire.beginTransmission(0x68);Wire.write(0x3B);Wire.endTransmission(false);
+  Wire.requestFrom(0x68,14,true);
+  int16_t d[6];
+  for(int i=0;i<3;i++){d[i]=(Wire.read()<<8)|Wire.read();Wire.read();Wire.read();}
+  for(int i=3;i<6;i++) d[i]=(Wire.read()<<8)|Wire.read();
+  float ss[6]={9.81/16384.0,9.81/16384.0,9.81/16384.0,PI/180.0/131.0,PI/180.0/131.0,PI/180.0/131.0};
+  for(int i=0;i<6;i++){float v=d[i]*ss[i];pack(v<0,1,p);pack(min(127,int(abs(v)*3)),7,p);}
+
+  float c=myMax.CurrentRead(), v=myMax.VoltageRead();
+  pack(uint32_t(v),4,p); pack(uint32_t((v-int(v))*100),7,p);
+  pack(uint32_t(c),2,p);   pack(uint32_t((c-int(c))*100),7,p);
+
+  if(gpsSerial.available()) gps.encode(gpsSerial.read());
+  if(gps.location.isUpdated()){
+    pack(gps.date.month(),4,p); pack(gps.date.day(),5,p); pack(gps.date.year()-2000,6,p);
+    pack(gps.time.hour(),5,p);  pack(gps.time.minute(),6,p); pack(gps.time.second(),6,p);
+    auto ec=[&](double x,int w,int f){pack(x<0,1,p); if(x<0)x=-x; pack(uint32_t(x),w,p); pack(uint32_t((x-int(x))*(1UL<<f)),f,p);};
+    ec(gps.location.lat(),8,23); ec(gps.location.lng(),8,23); ec(gps.altitude.meters(),16,7);
+  }
+}
+
+void toHex(){
+  for(int i=0;i<27;i++){
+    uint8_t b=pkt[i];
+    hexOut[2*i]   = (b>>4)<10?('0'+(b>>4)):('A'+(b>>4)-10);
+    hexOut[2*i+1] = (b&0x0F)<10?('0'+(b&0x0F)):('A'+(b&0x0F)-10);
+  }
+  hexOut[54]=0;
+}
+
+void setup(){
+  Serial.begin(115200);
+  gpsSerial.begin(9600);
+  Wire.begin(); SPI.begin();
+  mag.begin(); mpu.begin();
+  mpu.setAccelerometerRange(MPU6050_RANGE_2_G);
+  mpu.setGyroRange(MPU6050_RANGE_250_DEG);
+  SD.begin(CS_PIN);
+}
+
+void loop(){
+  if(millis()-lastT>3000){
+    lastT = millis();
+    build();
+    toHex();
+    Serial.println(hexOut);  // Existing line
+
+    
+    SdFile f;
+    if(f.open("datalog.txt", O_CREAT | O_WRITE | O_APPEND)){
+      f.println(hexOut);
+      f.sync();
+      f.close();
+    }
+
+    // Extract hex characters 19 to 23 (i.e., hexOut[18] to hexOut[22])
+    char poopyOut[11]; // "POOPY" + 5 chars + null terminator
+    strcpy(poopyOut, "POOPY");
+    strncpy(poopyOut + 5, hexOut + 18, 5); // Copy characters 18 to 22
+    poopyOut[10] = '\0'; // Null-terminate the string
+    Serial.println(poopyOut); // Print the "POOPY_____" line
+  }
+}
